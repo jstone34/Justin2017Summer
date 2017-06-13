d7d2e9935573108e85bf597bc24e9f49aec7f775
Justin <jstone34@student.gsu.edu>
2017-06-13 19:52:25 -0400

    6/13/17 Updates.
    Recent updates after in class task

--- app/src/main/java/com/example/justin/justin2017summer/fragment/DemoFragment.java
+++ app/src/main/java/com/example/justin/justin2017summer/fragment/DemoFragment.java
@@ -1,15 +1,20 @@
 package com.example.justin.justin2017summer.fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import java.util.ArrayList;
 
+import com.example.justin.justin2017summer.LaunchModeActivity;
 import com.example.justin.justin2017summer.R;
+import com.example.justin.justin2017summer.ViewPagerActivity;
 import com.example.justin.justin2017summer.adapter.ListNormalAdapter;
 
 /**
@@ -37,7 +42,7 @@ public class DemoFragment extends Fragment {
         contentList.add("A");
         contentList.add("B");
         contentList.add("B");
-        contentList.add("D");
+        contentList.add("Launch Mode");
         contentList.add("E");
         contentList.add("F");
         contentList.add("G");
@@ -71,6 +76,28 @@ public class DemoFragment extends Fragment {
         ListNormalAdapter adapter;
         adapter = new ListNormalAdapter(this.getContext(),contentList);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        Intent intent = new Intent(getActivity(), ViewPagerActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getActivity(), ScaleTypeActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case 6:
+                        Intent intent6 = new Intent(getActivity(), LaunchModeActivity.class);
+                        startActivity(intent6);
+                        break;
+
+                    default:
+                }
+            }
+        });
         return view;
     }
 
